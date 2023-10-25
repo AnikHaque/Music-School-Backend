dd5dffb4b33d8ce094238501341a708f7c033492
AnikHaque <ekramul15-811@diu.edu.bd>
2023-10-25 12:12:06 +0600

    payment added

--- index.js
+++ index.js
@@ -314,14 +314,8 @@ async function run() {
       res.send({ insertResult, deleteResult });
     })
 
-
-
-    // Send a ping to confirm a successful connection
-    // await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
-    // Ensures that the client will close when you finish/error
-    // await client.close();
   }
 }
 run().catch(console.dir);
