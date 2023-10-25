da37a4f5390b23cce378999ca7a7bea70d8bfeb6
AnikHaque <ekramul15-811@diu.edu.bd>
2023-10-25 12:08:19 +0600

    connection url

--- index.js
+++ index.js
@@ -44,10 +44,7 @@ const client = new MongoClient(uri, {
 
 async function run() {
   try {
-    // Connect the client to the server	(optional starting in v4.7)
-    // await client.connect();
-
-
+   
     const usersCollection = client.db("summer-camp-music").collection("users");
     const classesCollection = client.db("summer-camp-music").collection("classes");
     const instructorsCollection = client.db("summer-camp-music").collection("instructors");
